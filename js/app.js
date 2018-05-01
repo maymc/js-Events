60ebd330d33b13533a47a2c6b36aa66dfb416f83
May Chen <maychen@Mays-MacBook-Pro.local>
2018-05-01 00:01:55 -1000

    completed feature2 exercises

--- js/app.js
+++ js/app.js
@@ -6,15 +6,30 @@ The function will add the following content inside the paragraph with the id of 
 
 var bacon = "Shoulder turducken brisket, kevin swine andouille tri-tip salami tail ham sausage pork loin. Ribeye short loin rump kielbasa pork. Capicola short loin turducken corned beef tongue, chuck leberkas salami frankfurter. Kielbasa fatback pancetta, ground round meatball turducken jowl ribeye alcatra sirloin bacon corned beef beef ribs short loin. Pork belly spare ribs biltong corned beef meatball short ribs tongue alcatra swine drumstick. Biltong shankle kevin, cupim sirloin bresaola brisket. Tail pork belly biltong ball tip tri-tip, pig jerky cow pastrami prosciutto ;ground round bacon capicola tongue meatball.";
 
+function moreContent() {
+	var moreElem = document.getElementById("more");
+	moreElem.innerHTML = bacon;
+}
+
 
 //2. HTTP
 /*Create a function named `lessContent` that will initiate the `Show Less` link after clicking on it.
 
 The function will hide the contents in the pargraph with the id of `less` after clicking on the `Show Less` link.*/
 
+function lessContent(){
+	var lessElem = document.getElementById("less");
+	lessElem.style.display = "none";
+}
+
 
 //3. Tacocat, The Original Palindrome King
 /*Create a function named `zoom` that will increase the font size of the paragraph with the id of `biggie` after hovering your mouse over it. Increae the font size to 150%*/
+
+function zoom(){
+	var biggieElem = document.getElementById("biggie");
+	biggieElem.style.fontSize = "150%"
+}
 
 
 //4. McDonalds
@@ -22,28 +37,73 @@ The function will hide the contents in the pargraph with the id of `less` after 
 
 Next, create a function named valueMenu that will display your favorite items in the paragraph the the id of `menu` after clicking on the showMenu paragraph.*/
 
+var menuItems = ["fries", "mcChicken", "mcFlurry"];
+
+function valueMenu(){
+	document.getElementById("menu").innerHTML = menuItems;
+}
 
 //5. Gin.
 /*Create a function named redFace that will change the paragraph text to red and a font size of 30px after clicking on the text.*/
+
+function redFace(){
+	document.getElementById("drink").style.color = "red";
+	document.getElementById("drink").style.fontSize = "30px";
+}
 
 
 //6. Peanut Butter Cup Oreos
 /*Create a function `showPrice` that will add the price of `$5.55` inside the paragraph with the id `price` after hovering your mouse over the paragraph.*/
 
+function showPrice(){
+	document.getElementById("price").innerHTML = "$5.55";
+}
 
 //7. Mr. Buttons
 /*Add an Event Listener to the button that will display `myQuote` inside the paragraph with the id of `displayQuote` after the button is clicked.*/
 
 var myQuote = "Our lives are defined by opportunities; even the ones we miss.";
 
+var buttonElem = document.getElementById("Benjamin");
+buttonElem.addEventListener("click", quoteGenerate);
+
+function quoteGenerate(){
+	document.getElementById("displayQuote").innerHTML = myQuote;
+}
 
 //8. Say It again, Randomly
 /*Create a function that will generate a random quote from the variable below after clicking on the button.*/
 
 var quotes = ["It's a funny thing about comin' home. Looks the same, smells the same, feels the same. You'll realize what's changed is you.", "Momma? Momma? Some days, I feel different than the day before.", "Some people, were born to sit by a river. Some get struck by lightning. Some have an ear for music. Some are artists. Some swim. Some know buttons. Some know Shakespeare. Some are mothers. And some people, dance.", "For what it's worth, it's never too late to be whoever you want to be."];
 
+var quoteNum = Math.floor(Math.random()*3);
+console.log(quoteNum);
+
+var btnElem = document.getElementById("random");
+btnElem.addEventListener("click", randomQuote);
+
+function randomQuote(){
+	document.getElementById("displayQuotes").innerHTML = quotes[quoteNum];
+}
 
 
 //9. Unlock the Secret to Financial Freedom
 /*Create an event listener that will show and hide the message when clicking on the button.
 */
+
+var btnElem2 = document.getElementById("showHide");
+btnElem2.addEventListener("click", showHideSecret);
+
+function showHideSecret(){
+	var hide = document.getElementById("showmoney");
+	if(hide.style.display === "none")
+		hide.style.display = "block";
+	else
+		hide.style.display = "none";
+}
+
+
+
+
+
+
